20f5fb86f86f26d6edd23a711cbe5634d2584622
JohnCarloBacs <johncarlobacolod21@gmail.com>
2026-04-09 20:56:47 +0800

    done adding models and controllers for courses and users

---
+++ backend/app/Models/Course.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    public function teacher () {
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
+}
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -46,4 +46,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function courses () {
+        return $this->hasMany(Course::class, 'teacher_id');
+    }
 }
