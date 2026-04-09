4f08e6265ce689de4886852b03d5a965823feb40
JohnCarloBacs <johncarlobacolod21@gmail.com>
2026-04-09 21:06:30 +0800

    fixing the modal frame scrolls even though the modal is active

--- frontend/src/components/Modal/ModalFrame.jsx
+++ frontend/src/components/Modal/ModalFrame.jsx
@@ -1,7 +1,18 @@
 import { createPortal } from "react-dom";
 import "./ModalFrame.css";
+import { useEffect } from "react";
 
 const ModalFrame = ({ children, onClose }) => {
+  useEffect(() => {
+    // Disables the scrolling when the modal mounts
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      // Enables the scrolling when the modal unmounts
+      document.body.style.overflow = "unset";
+    };
+  }, []);
+
   return createPortal(
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
